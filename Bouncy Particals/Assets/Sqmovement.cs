d03502f2ddd52f584f6758461d016e01daff0625
Alex Wang <wangtianyoualex@gmail.com>
2019-09-19 13:21:20 -0400

    Week 2 Project
    Week 2 project

---
+++ Bouncy Particals/Assets/Sqmovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sqmovement : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    public float forceamount;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = new Vector2(3, 3);
+        rb = GetComponent<Rigidbody2D>();
+        forceamount = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+
+        {
+            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log("Clicked!" + worldPoint.x + "" + worldPoint.y);
+
+            Vector3 direction = worldPoint - transform.position;
+            rb.AddForce(new Vector2(direction.x, direction.y)*forceamount, ForceMode2D.Impulse);
+
+        }
+    }
+}
