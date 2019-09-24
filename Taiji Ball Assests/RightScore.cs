726cd615e2bae6ce0d6fafe5ab1fd57f452ca4af
Alex Wang <wangtianyoualex@gmail.com>
2019-09-24 09:02:12 -0400

    Week 4 TaiJi Ball

---
+++ Taiji Ball Assests/RightScore.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RightScore : MonoBehaviour
+{
+   
+    public Text RightPlayerScore;
+    public float Score = 0;
+
+    public GameObject LeftPlayer;
+
+    public GameObject RightWinScreen;
+
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RightPlayerScore.text = Score.ToString();
+
+        if (Score >= 7)
+
+        {
+            //LeftWinScreen.transform.position = new Vector2(-3.86f, 13.6f);
+            RightWinScreen.transform.Translate(new Vector2(0, -0.3f));
+            if (RightWinScreen.transform.position.y <= 1.14f)
+            {
+                RightWinScreen.transform.position = new Vector2(3.86f, 1.14f);
+                //Time.timeScale = 0;
+            }
+
+        }
+    }
+
+   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "LeftPlayer")
+        { AddScore();
+
+            LeftPlayer.transform.position = new Vector2(Random.Range(-2.0f,-4.0f), 2);
+            LeftPlayer.GetComponent<Rigidbody2D>().Sleep();
+            
+        }
+    }
+    void AddScore()
+
+    {
+        Score++;
+    }
+}
---
+++ Taiji Ball Assests/wrapping.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class wrapping : MonoBehaviour
+{
+    float x = 8.0f;
+    Rigidbody2D rb;
+
+
+
+   // public Text ScoreLeft;
+    //public Text ScoreRight;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x >= x)
+        {
+            transform.position = new Vector2(-x, transform.position.y);
+
+        }
+        else if (transform.position.x <= -x)
+        {
+            transform.position = new Vector2(x, transform.position.y);
+        }
+
+      //  ScoreLeft.text = LeftScore.ToString();
+      //  ScoreRight.text = RightScore.ToString();
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        Destroy(this.gameObject);
+       // Destroy(this.gameObject.FindWithTag("RightBullet"));
+
+        //Destroy(GameObject.FindWithTag("LeftPlayer"));
+
+        // Destroy(GameObject.FindWithTag("RightPlayer"));
+
+        //GameObject L = Instantiate(leftPlayer) as GameObject;
+        // L.transform.position = new Vector2(0, 0);
+
+    }
+
+    //  void LeftAddScore()
+    // {
+    //var countplayerLeft = GameObject.FindGameObjectsWithTag("LeftPlayer").Length;
+    // if (countplayerLeft < 1)
+    //{
+    //  RightScore++;
+
+    //  GameObject l = Instantiate(leftPlayer) as GameObject;
+    //  l.transform.position = new Vector2(-4, 0);
+
+}
+
+      //  var countplayerRight = GameObject.FindGameObjectsWithTag("RightPlayer").Length;
+       // if (countplayerRight < 1)
+      //  {
+       //     LeftScore++;
+
+      //  }
+  //  }
+
+//}
