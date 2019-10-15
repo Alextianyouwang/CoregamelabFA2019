66a3dd62a45a119d8db7e29661969069f59ea201
Alex Wang <wangtianyoualex@gmail.com>
2019-10-15 11:46:29 -0400

    Quiz1

---
+++ Quiz1/Assets/Pltmove2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pltmove2 : MonoBehaviour
+{
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Instantiate(Platform, transform.position, transform.rotation);
+        transform.position = new Vector2(0, 1);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.mousePosition.x;
+        transform.position = new Vector2(0, 0);
+        transform.rotation = Quaternion.Euler(0, 0, (x - 300) / 10);
+    }
+
+}
